b724d04019ab67e8bf194ab427c9ede8c928546e
Anne Zhou <anne.zhou920@gmail.com>
2019-03-17 19:51:52 -0400

    first commit

---
+++ about_hashes.rb
@@ -0,0 +1,128 @@
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+class AboutHashes < Neo::Koan
+  def test_creating_hashes
+    empty_hash = Hash.new
+    assert_equal Hash, empty_hash.class
+    assert_equal({}, empty_hash)
+    assert_equal 0, empty_hash.size
+  end
+
+  def test_hash_literals
+    hash = { :one => "uno", :two => "dos" }
+    assert_equal 2, hash.size
+  end
+
+  def test_accessing_hashes
+    hash = { :one => "uno", :two => "dos" }
+    assert_equal "uno", hash[:one]
+    assert_equal "dos", hash[:two]
+    assert_equal nil, hash[:doesnt_exist]
+  end
+
+  def test_accessing_hashes_with_fetch
+    # https://stackoverflow.com/questions/16569409/fetch-vs-when-working-with-hashes
+
+    hash = { :one => "uno" }
+    assert_equal "uno", hash.fetch(:one)
+    assert_raise(KeyError) do
+      hash.fetch(:doesnt_exist)
+    end
+
+    # THINK ABOUT IT:
+    #
+    # Why might you want to use #fetch instead of #[] when accessing hash keys?
+  end
+
+  def test_changing_hashes
+    hash = { :one => "uno", :two => "dos" }
+    hash[:one] = "eins"
+
+    expected = { :one => "eins", :two => "dos" }
+    assert_equal expected, hash
+
+    # Bonus Question: Why was "expected" broken out into a variable
+    # rather than used as a literal?
+    # https://stackoverflow.com/a/7533906/10443082
+    # we use variabe because Ruby thinks that { ... } is a block
+  end
+
+  def test_hash_is_unordered
+    hash1 = { :one => "uno", :two => "dos" }
+    hash2 = { :two => "dos", :one => "uno" }
+
+    assert_equal true, hash1 == hash2
+  end
+
+  def test_hash_keys
+    hash = { :one => "uno", :two => "dos" }
+    assert_equal 2, hash.keys.size
+    assert_equal true, hash.keys.include?(:one)
+    assert_equal true, hash.keys.include?(:two)
+    assert_equal Array, hash.keys.class
+  end
+
+  def test_hash_values
+    hash = { :one => "uno", :two => "dos" }
+    assert_equal 2, hash.values.size
+    assert_equal true, hash.values.include?("uno")
+    assert_equal true, hash.values.include?("dos")
+    assert_equal Array, hash.values.class
+  end
+
+  def test_combining_hashes
+    hash = { "jim" => 53, "amy" => 20, "dan" => 23 }
+    new_hash = hash.merge({ "jim" => 54, "jenny" => 26 })
+
+    assert_equal true, hash != new_hash
+
+    expected = { "jim" => 54, "amy" => 20, "dan" => 23, "jenny" => 26 }
+    assert_equal true, expected == new_hash
+  end
+
+  def test_default_value
+    hash1 = Hash.new
+    hash1[:one] = 1
+
+    assert_equal 1, hash1[:one]
+    assert_equal nil, hash1[:two]
+
+    hash2 = Hash.new("dos")
+    hash2[:one] = 1
+
+    assert_equal 1, hash2[:one]
+    assert_equal "dos", hash2[:two] # when you index into something that doesn't exist, it will default to value passed to new
+  end
+
+  def test_default_value_is_the_same_object
+    # << is called shovel operator
+    # https://stackoverflow.com/questions/35833583/why-does-the-test-default-value-is-the-same-object-in-about-hashes-rb-of-ruby-ko
+    # hash[:one] doesn't exist, so "uno" gets pushed to empty array referenced by hash
+    # hash[:two] doesn't exist, so "dos" gets pushed into that same array ^
+    # you are pushing to a key that doesn’t exist, so it get’s pushed to the default value, the original array.
+
+    hash = Hash.new([])
+
+    hash[:one] << "uno"
+    hash[:two] << "dos"
+
+    assert_equal ["uno", "dos"], hash[:one]
+    assert_equal ["uno", "dos"], hash[:two]
+    assert_equal ["uno", "dos"], hash[:three]
+
+    assert_equal true, hash[:one].object_id == hash[:two].object_id
+  end
+
+  def test_default_value_with_block
+    # you are putting a block that creates an array as the default object.
+    # Every unclaimed key in the Hash, will use the block to create a new empty array at that key
+    hash = Hash.new {|hash, key| hash[key] = [] }
+
+    hash[:one] << "uno"
+    hash[:two] << "dos"
+
+    assert_equal ["uno"], hash[:one]
+    assert_equal ["dos"], hash[:two]
+    assert_equal [], hash[:three]
+  end
+end
